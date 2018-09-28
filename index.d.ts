5d229ca4f75d57abb8c5e61733a2309f93585898
theonetheycallneo <anthony.wentzel@gmail.com>
2018-09-28 14:43:18 -0700

    fix: types and js

---
+++ index.d.ts
@@ -0,0 +1,22 @@
+declare module 'react-native-analytics' {
+  export default class Segment {
+    static setup(
+      segmentIOWriteKey: string,
+      flushEverySecondsCount: number,
+      shouldUseLocationServices?: boolean
+    ): void
+    static identify(userId: string, traits?: any, options?: any): void
+    static track(eventName: string, eventProperties?: any, options?: any): void
+    static screen(
+      screenName: string,
+      screenProperties?: any,
+      options?: any
+    ): void
+    static flush(): void
+    static reset(): void
+    static debug(isEnabled: boolean): void
+    static enable(): void
+    static disable(): void
+  }
+}
+
