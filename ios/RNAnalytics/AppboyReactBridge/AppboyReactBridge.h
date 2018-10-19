eb35fbfc28dfc767ac753c9fdf20b6c4754b18dd
theonetheycallneo <anthony.wentzel@gmail.com>
2018-10-18 17:23:28 -0700

    fix: ios crash

--- ios/RNAnalytics/AppboyReactBridge/AppboyReactBridge.h
+++
@@ -1,7 +0,0 @@
-#import <Foundation/Foundation.h>
-#import <React/RCTBridgeModule.h>
-
-// TODO (Brian) - Modify header/library search paths to be more generic or assume node installation
-@interface AppboyReactBridge : NSObject <RCTBridgeModule>
-
-@end
