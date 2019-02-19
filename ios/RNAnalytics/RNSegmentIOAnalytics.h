35f20ebe084f1564e676d6332c51bdd1438b6699
theonetheycallneo <anthony.wentzel@gmail.com>
2019-02-19 15:39:44 -0800

    fix(ios): appsflyer lifecycle events

--- ios/RNAnalytics/RNSegmentIOAnalytics.h
+++ ios/RNAnalytics/RNSegmentIOAnalytics.h
@@ -5,7 +5,13 @@
 //  Copyright (c) 2015 Fire Place Inc. All rights reserved.
 //
 
+#if __has_include(<React/RCTBridgeModule.h>) //ver >= 0.40
 #import <React/RCTBridgeModule.h>
+#import <React/RCTEventDispatcher.h>
+#else //ver < 0.40
+#import "RCTBridgeModule.h"
+#import "RCTEventDispatcher.h"
+#endif
 
 #import "AppboyKit.h"
 #import "IDFADelegate.h"
